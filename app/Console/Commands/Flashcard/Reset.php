3a4db631433c51d7b837ca2ea5f5c6a878760642
BehzadFazelasl <behzadfazelasl@teknasyon.com>
2022-02-07 09:25:39 +0300

    Add reset process option

---
+++ app/Console/Commands/Flashcard/Reset.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Console\Commands\Flashcard;
+
+use App\Models\Flashcard;
+use Illuminate\Console\Command;
+
+class Reset extends Command
+{
+    protected $hidden = true;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'flashcard:reset';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Reset the practice progress';
+
+    private MainMenu $parent;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(MainMenu $parent)
+    {
+        $this->parent = $parent;
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        system('clear');
+
+        $this->parent->line('Report of what you have done so far:');
+        $this->parent->line(sprintf('Total Number Of Questions : %s', $this->parent->getFlashcardService()->totalCount()));
+        $this->parent->line(sprintf('Answered : %s %%', $this->parent->getFlashcardService()->getAnsweredCardsPercentage()));
+        $this->parent->line(sprintf('Answered Correctly : %s %%', $this->parent->getFlashcardService()->getCorrectlyAnsweredCardsPercentage()));
+
+        if ($this->parent->confirm('Do you wish to continue resetting the process?')) {
+            $this->parent->getFlashcardService()->resetProgress();
+            system('clear');
+            $this->parent->info('Practice process has been reset!');
+        }
+
+        return 0;
+    }
+}
