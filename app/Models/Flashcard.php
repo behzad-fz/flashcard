cc0503ab45de0bac3191b56464287b6c932e281b
BehzadFazelasl <behzadfazelasl@teknasyon.com>
2022-02-09 17:02:58 +0300

    Add relations to models

--- app/Models/Flashcard.php
+++ app/Models/Flashcard.php
@@ -4,14 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Flashcard extends Model
 {
     use HasFactory;
 
-    protected $guarded = [];
+    protected $fillable = [
+        'question',
+        'answer',
+    ];
 
     const STATUS_CORRECT = 'Correct';
     const STATUS_INCORRECT = 'Incorrect';
     const STATUS_NOT_ANSWERED = 'Not Answered';
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public static function getAllWithCurrentUserStatus(int $userId)
+    {
+        return Flashcard::leftJoin('flashcard_user', function($join) use ($userId) {
+            $join->on('flashcards.id', '=', 'flashcard_user.flashcard_id')
+                ->where('flashcard_user.user_id', '=', $userId);
+        })
+        ->select('flashcards.id', 'flashcards.question', DB::Raw('IFNULL( `flashcard_user`.`status`, "Not Answered") as status'));
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+
 
 class User extends Authenticatable
 {
@@ -41,4 +41,34 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function cards()
+    {
+        return $this->belongsToMany(Flashcard::class)->withPivot('status');
+    }
+
+    public function answeredCards()
+    {
+        return $this->belongsToMany(Flashcard::class)->wherePivot('status', '!=', Flashcard::STATUS_NOT_ANSWERED);
+    }
+
+    public function notAnsweredCards()
+    {
+        return Flashcard::query()->whereNotIn('id', $this->answeredCards()->get()->pluck('id')->toArray());
+    }
+
+    public function correctlyAnsweredCards()
+    {
+        return $this->belongsToMany(Flashcard::class)->wherePivot('status', Flashcard::STATUS_CORRECT);
+    }
+
+    public function IncorrectlyAnsweredCards()
+    {
+        return $this->belongsToMany(Flashcard::class)->wherePivot('status', Flashcard::STATUS_INCORRECT);
+    }
+
+    public function hasAnsweredCorrectlyBefore($flashcardId): bool
+    {
+        return (bool) $this->correctlyAnsweredCards()->wherePivot('flashcard_id', $flashcardId)->first();
+    }
 }
